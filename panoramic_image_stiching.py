2bf021540078421d492dc7095926a2b6581658f8
wenjietseng <wenjietseng@gmail.com>
2018-04-17 21:20:17 +0800

    hw2

--- panoramic_image_stiching.py
+++ panoramic_image_stiching.py
@@ -37,7 +37,8 @@ class image_stiching(object):
         self.result_img = cv2.warpPerspective(self.img2, transform_array.dot(self.H), 
                                     (x_max-x_min, y_max-y_min))
 
-        self.img1 = self.alpha_blend(self.img1, self.result_img, transform_dist)
+        # blending here
+        # self.img1, self.result_img = self.pyramid_blending(self.img1, self.result_img, transform_dist)
         # original code directly paste two imgs together
         self.result_img[transform_dist[1]:w1+transform_dist[1], 
                     transform_dist[0]:h1+transform_dist[0]] = self.img1
@@ -54,7 +55,7 @@ class image_stiching(object):
 
         foreground = img_f[ :, w1-fix_width:w1,]
         background = img_b[ transform_dist[0]:h1+transform_dist[0], w1+transform_dist[1]:w1+transform_dist[1]+fix_width,]
-        print(w1, h1)
+        # print(w1, h1)
         # print(foreground.shape, background.shape)
         kernel = np.ones((5, 5), np.uint8)
         foreground_gray = cv2.cvtColor(foreground, cv2.COLOR_BGR2GRAY)
@@ -69,3 +70,53 @@ class image_stiching(object):
         img_f[:, w1-fix_width:w1] = output
         # img_b[transform_dist[0]:h1+transform_dist[0], w1+transform_dist[1]:w1+transform_dist[1]+fix_width] = output
         return img_f
+
+    def pyramid_blending(self, img_f, img_b, transform_dist, fix_width=100):
+        # A is forground, B is background
+        # generate Gaussian pyramid for A
+        h1, w1 = img_f.shape[:2]
+
+        A = img_f[ :, w1-fix_width:w1,]
+        B = img_b[ transform_dist[0]:h1+transform_dist[0], w1+transform_dist[1]:w1+transform_dist[1]+fix_width,]
+        G = A.copy()
+        gpA = [G]
+        for i in range(6):
+            G = cv2.pyrDown(G)
+            gpA.append(G)
+        # generate Gaussian pyramid for B
+        G = B.copy()
+        gpB = [G]
+        for i in range(6):
+            G = cv2.pyrDown(G)
+            gpB.append(G)
+        # generate Laplacian Pyramid for A
+        lpA = [gpA[5]]
+        for i in range(5,0,-1):
+            GE = cv2.pyrUp(gpA[i])
+            L = cv2.subtact(gpA[i-1],GE)
+            lpA.append(L)
+
+        # generate Laplacian Pyramid for B
+        lpB = [gpB[5]]
+        for i in range(5,0,-1):
+            GE = cv2.pyrUp(gpB[i])
+            L = cv2.subtract(gpB[i-1],GE)
+            lpB.append(L)
+
+        # Now add left and right halves of images in each level
+        LS = []
+        for la,lb in zip(lpA,lpB):
+            rows,cols,dpt = la.shape
+            ls = np.hstack((la[:,0:cols/2], lb[:,cols/2:]))
+            LS.append(ls)
+
+        # now reconstruct
+        ls_ = LS[0]
+        for i in range(1,6):
+            ls_ = cv2.pyrUp(ls_)
+            ls_ = cv2.add(ls_, LS[i])
+        # image with direct connecting each half
+        real = np.hstack((A[:,:cols/2],B[:,cols/2:]))
+        A[:, w1-fix_width:w1] = ls_
+        B[transform_dist[0]:h1+transform_dist[0], w1+transform_dist[1]:w1+transform_dist[1]+fix_width] = ls_
+        return A, B
