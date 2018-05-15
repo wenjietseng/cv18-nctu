16e1d7ffbb608cae9cb4cbfd7bedec59c163c8df
LiYangWang95 <liyangwang95@gmail.com>
2018-05-15 14:29:38 +0800

    Updated. Test image read in, bag of SIFT done

--- categorize-images/task_2.py
+++ categorize-images/task_2.py
@@ -11,6 +11,7 @@
 from sklearn import cluster, metrics
 import numpy as np
 import cv2, os
+import matplotlib.pyplot as plt
 
 # Transform the clustered result into histogram
 def clusterToHistogram(clt):
@@ -20,10 +21,7 @@ def clusterToHistogram(clt):
     # Create histogram
     (hist, _) = np.histogram(clt.labels_, bins = numLabels)
 
-    # Normalize the histogram
-    # hist = hist.astype("float")
-    # hist /= hist.sum()
-
+    # Return histogram
     return hist
 
 # Function used to read data
@@ -56,49 +54,83 @@ def dataRead():
     # Print out the number of training images
     # print("Number of training images: %d " % len(train_img_list))
 
-    # Return data
-    return train_img_list
+    # Path to test data
+    # train_path = "./hw4_data/test/"
+    test_path = "./hw4_data/test/Bedroom"
+    test_img_list = []
 
-    # test_path = "./hw4_data/test/"
-    # test_img_list = []
+    # Read the folder names
+    # img_dirs = [d for d in os.listdir(test_path) if not d.startswith('.')]
+
+    # Read all the file names in folders, one folder by one folder
     # img_names = [os.listdir(test_path + d) for d in img_dirs if not d.startswith('.')]
+    img_names = [n for n in os.listdir(test_path) if not n.startswith('.')]
 
+    # Read all the images
     # for d_idx in range(len(img_names)):
     #     for name in img_names[d_idx]:
     #         if not name.startswith('.'):
-    #             # print(test_path + img_dirs[d_idx] + '/' + name)
     #             img = cv2.imread(test_path + img_dirs[d_idx] + '/' + name, 0)
     #             test_img_list.append(img)
-    # print("Number of testing images: %d " % len(test_img_list))
+    
+    for d_idx in range(len(img_names)):
+        name = img_names[d_idx]
+        if not name.startswith('.'):
+            img = cv2.imread(test_path + '/' + name, 0)
+            test_img_list.append(img)
+    
+    # Print out the number of training images
+    # print("Number of training images: %d " % len(train_img_list))
 
-# img = cv2.imread("./hw4_data/train/Bedroom/image_0002.jpg", cv2.IMREAD_GRAYSCALE)
+    # Return data
+    return train_img_list, test_img_list
+
+# Generate bags of SIFT descriptors
+def bagOfSIFT(train_img_list, test_img_list):
+    # Create extractor
+    extractor = cv2.xfeatures2d.SIFT_create()
+
+    # TRAIN IMAGE
+    # Compute descriptors, and store into descriptor array
+    train_descriptors = []
+    for img_idx in range(len(train_img_list)):
+        _, descriptors = extractor.detectAndCompute(train_img_list[img_idx], None)
+        train_descriptors.append(descriptors)
+
+    # Do k-means clustering
+    # Initiate KMeans
+    clt = cluster.KMeans(n_clusters = 4, random_state = 0)
+
+    # Cluster the histograms
+    train_hist = []
+    for clt_idx in range(len(train_descriptors)):
+        clt.fit(train_descriptors[clt_idx])
+
+        # Transform the clustered result into histogram
+        train_hist.append(clusterToHistogram(clt))
+
+    # TEST IMAGE
+    # Compute descriptors, and store into descriptor array
+    test_descriptors = []
+    for img_idx in range(len(test_img_list)):
+        _, descriptors = extractor.detectAndCompute(test_img_list[img_idx], None)
+        test_descriptors.append(descriptors)
+
+    # Cluster the histograms
+    test_hist = []
+    for clt_idx in range(len(test_descriptors)):
+        clt.fit(test_descriptors[clt_idx])
+
+        # Transform the clustered result into histogram
+        test_hist.append(clusterToHistogram(clt))
+
+    # Return histograms
+    return train_hist, test_hist
 
 # Read in all data
-train_img_list = dataRead()
-print(len(train_img_list))
-
-# Create extractor
-extractor = cv2.xfeatures2d.SIFT_create()
-
-# Compute descriptors, and store into descriptor array
-train_descriptors = []
-for img_idx in range(len(train_img_list)):
-    _, descriptors = extractor.detectAndCompute(train_img_list[img_idx], None)
-    train_descriptors.append(descriptors)
-
-print(len(train_descriptors))
-
-# Do k-means clustering
-# Initiate KMeans
-clt = cluster.KMeans(n_clusters = 4, random_state = 0)
-
-# Cluster the histograms
-train_hist = []
-for clt_idx in range(len(train_descriptors)):
-    clt.fit(train_descriptors[clt_idx])
-
-    # Transform the clustered result into histogram
-    train_hist.append(clusterToHistogram(clt))
-
+train_img_list, test_img_list = dataRead()
+train_hist, test_hist = bagOfSIFT(train_img_list, test_img_list)
+    
 # Show the histogram
-print(train_hist[0])
+print(len(train_hist))
+print(len(test_hist))
