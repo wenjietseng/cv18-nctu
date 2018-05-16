559d31b0a786928c80dc71560be9337aaaab8e48
wenjietseng <wenjietseng@gmail.com>
2018-05-16 17:19:13 +0800

    CNN

--- categorize-images/task_4/main.py
+++ categorize-images/task_4/main.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os
 import sys
 import torch
+import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 
@@ -13,16 +14,44 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
-import PIL
+from PIL import Image
 
 from models import WJNet
-
+from utils import *
 
 # 1. Loading images and preprocessing (center crop, resize, normalizing, padding zero, random flip)
+print('====> Loading Data ')
+train_dataset = ImageDataset(root_dir='../hw4_data', mode='train',
+                             transform=transforms.Compose([
+                                        transforms.CenterCrop(220),
+                                        transforms.Resize(254),
+                                        transforms.Pad(1, fill=0),
+                                        transforms.RandomHorizontalFlip(),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize((0.5,), (0.5,)),
+                             ]))
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=2)
+
+test_dataset = ImageDataset(root_dir='../hw4_data', mode='test',
+                            transform=transforms.Compose([
+                                      transforms.CenterCrop(220),
+                                      transforms.Resize(256), 
+                                      transforms.ToTensor(), 
+                                      transforms.Normalize((0.5,), (0.5,)),
+                            ]))
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=2)
+
+classes = [d for d in os.listdir('../hw4_data/train') if not d.startswith('.')]
+
+print('====> Complete loading data!')
 
 # 2. Define network, models are stored in model.py
+net = WJNet()
 
 # 3. Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 60], gamma=0.1)
 
 # Repeat 100 epochs
 #   4. Training
---
+++ categorize-images/task_4/utils.py
@@ -0,0 +1,52 @@
+import os
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+
+class ImageDataset(Dataset):
+    """ A torch Dataset class for hw4 """
+    def __init__(self, root_dir, mode, transform=None):
+        """
+        Args:
+            root_dir - root dir for all images
+            transform - optional transform to be applied on a sample
+        """
+        self.root_dir = root_dir
+        self.mode = mode
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        train_path = os.path.join(self.root_dir, "train")
+        test_path = os.path.join(self.root_dir, "test")
+        img_dirs = [d for d in os.listdir(train_path) if not d.startswith('.')]
+        out_list = []
+        labels = None
+
+        if self.mode == 'train':
+            # train_img_list = []
+            train_img_names = [os.listdir(os.path.join(train_path, d)) for d in img_dirs if not d.startswith('.')]
+            for i in range(len(img_dirs)):
+                for name in train_img_names[i]:
+                    if not name.startswith('.'):
+                        out_list.append(os.path.join(train_path, img_dirs[i], name))
+            labels = np.repeat(np.arange(15), 100)
+        elif self.mode == 'test':        
+            # test_img_list = []
+            test_img_names = [os.listdir(os.path.join(test_path, d)) for d in img_dirs if not d.startswith('.')]
+            for i in range(len(img_dirs)):
+                for name in test_img_names[i]:
+                    if not name.startswith('.'):
+                        out_list.append(os.path.join(test_path, img_dirs[i], name))
+            labels = np.repeat(np.arange(15), 10)
+        else:
+            raise ValueError("mode argument has to be 'train' or 'test'.")
+        
+    
+        img = Image.open(out_list[idx])
+        label = labels[idx]
+        sample = {'image': img, 'label':label}
+
+        if self.transform:
+            sample['image'] = self.transform(sample['image'])
+
+        return sample
