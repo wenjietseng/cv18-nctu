07213f9b77831c5e723b0520309ee9518bfa0abb
wenjietseng <wenjietseng@gmail.com>
2018-05-17 22:36:42 +0800

    check size

--- categorize-images/task_4/models.py
+++ categorize-images/task_4/models.py
@@ -4,11 +4,11 @@ import torch.nn.functional as F
 class WJNet(nn.Module):
     def __init__(self):
         super(WJNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 4, 16)
+        self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(4, 16, 16)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.pool2 = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(16 * 16 * 16, 128)
+        self.fc1 = nn.Linear(16 * 5 * 5, 128)
         self.fc2 = nn.Linear(128, 100)
         self.fc3 = nn.Linear(100, 15)
 
@@ -27,7 +27,7 @@ class WJNet(nn.Module):
         print('pool2 finish')
         print(x.size())
         # flatten
-        # x = x.view(-1, self.num_flat_features(x))
+        x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
