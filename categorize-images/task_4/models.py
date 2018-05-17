96cc232823f18a8ed85cc90b7f2180f5169936c8
wenjietseng <wenjietseng@gmail.com>
2018-05-17 22:37:54 +0800

    check size

--- categorize-images/task_4/models.py
+++ categorize-images/task_4/models.py
@@ -32,3 +32,12 @@ class WJNet(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        print(num_features)
+        return num_features
+
