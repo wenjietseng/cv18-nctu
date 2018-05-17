dba870b4d6416cbfa37191acf59a31fd35778e3b
wenjietseng <wenjietseng@gmail.com>
2018-05-18 02:03:30 +0800

    finish cnn

--- categorize-images/task_4/resnet.py
+++ categorize-images/task_4/resnet.py
@@ -65,15 +65,15 @@ class Bottleneck(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=15):
         super(ResNet, self).__init__()
-        self.in_planes = 1
+        self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(1, 1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(1)
-        self.layer1 = self._make_layer(block, 2, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 4, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 8, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 16, num_blocks[3], stride=2)
-        self.linear = nn.Linear(16*block.expansion, num_classes)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -90,7 +90,7 @@ class ResNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
-        out = out.view(1024, -1)#out.size(0), -1)
+        out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
